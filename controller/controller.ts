162513cd91640d2fda4e48ca95876c0cb10972b6
kresna <kresnafti2013@gmail.com>
2023-08-18 01:54:30 +0700

    update endpoint func

--- controller/controller.ts
+++ controller/controller.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import Expenses from '../model/Expenses';
 import { validationResult } from "express-validator";
+import { connection } from '../connection';
 
 let models:Array<any> = [];
 
@@ -17,49 +18,71 @@ const expenseController = {
 
   getExpense: async (req: express.Request, res: express.Response) => {
     try {
-      if (models.length <= 0) {
-        res.status(200).send({ 
-          responCode: 200,
-          status: 'success',
-          message: 'Data empty',
-          payloads: models
+      connection.query(`SELECT * FROM tb_expense;`, function (error, results) {
+        if(error) throw error;
+        if(results.length > 0) {
+          res.status(200).send({ 
+            responCode: 200,
+            status: 'success', 
+            message: 'Berhasil ambil data!',
+            payloads: results 
         });
-      } else {
-        res.status(200).send({ 
-          responCode: 200,
-          status: 'success',
-          message: 'Success get data',
-          payloads: models
+        } else {
+          res.status(404).send({ 
+            responCode: 404,
+            status: 'success', 
+            message: 'Data not found',
+            payloads: results 
         });
-      }
+        }
 
-    } catch (error: any) {
+      });
+    } catch (error:any) {
       res.status(505).send({ 
         responCode: 505,
         status: 'failed',
         message: 'Network Error',
-        payloads: error.message
+        payloads: error.message 
       });
     }
   },
   getExpensebyId: async (req: express.Request, res: express.Response) => {
     try {
       let ids = req.params.id
-      let objIndex = models.findIndex((obj => obj.id == `${ids}`));
-      if (objIndex !== -1) {
+      connection.query(`
+        SELECT 
+          u.id, 
+          u.name, 
+          u.address , 
+        SUM(CASE WHEN e.type = 'Cash In' THEN e.jumlah ELSE 0 END) AS income,
+        SUM(CASE WHEN e.type = 'Cash Out' THEN e.jumlah ELSE 0 END) AS expense
+        FROM tb_users AS u 
+        JOIN tb_expense AS e 
+        ON e.id_user = u.id
+        WHERE e.id_user = ${ids}
+        GROUP BY u.id`, function (error, results) {
+          
+        if(error) throw error;
+        if(results.length > 0) {
+          let parsed = JSON.parse(JSON.stringify(results))
+          let balance = parsed[0].income - parsed[0].expense
+          parsed[0]["balance"] = balance
 
-        res.status(200).send({ 
-          responCode: 200,
-          status: 'success',
-          payloads: models[objIndex]
+          res.status(200).send({ 
+            responCode: 200,
+            status: 'success', 
+            message: 'Berhasil ambil data!',
+            payloads: parsed 
         });
-      } else {
+        } else {
           res.status(404).send({ 
-          responCode: 404,
-          status: 'success',
-          message: 'Data not Found'
-        });
-      }
+            responCode: 404,
+            status: 'success', 
+            message: 'Data not found',
+            payloads: results 
+         });
+        }
+      });
 
     } catch (error: any) {
       res.status(505).send({ 
@@ -73,31 +96,25 @@ const expenseController = {
   addExpense: (req: express.Request, res: express.Response) => {
 
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          success: false,
-          errors: errors.array(),
-        });
-      } else {
-
-        let id = "id" + Math.random().toString(16).slice(2)
-        models.push(collectData(id, req.body.type == '1' ? 'Cash In' : 'Cash Out', req.body.name, req.body.details, parseInt(req.body.jumlah) ))
+      // console.log(req.body)
+      let id = "id" + Math.random().toString(16).slice(2)
+      connection.query(`INSERT INTO tb_expense (id,id_user,type, name, details, jumlah) VALUES(?,?,?,?,?,?);`, [id, req.body.id_user, req.body.type == '1' ? 'Cash In' : 'Cash Out', req.body.name, req.body.details, parseInt(req.body.jumlah)], 
+      function (error, results) {
+        if(error) throw error;  
         res.status(200).send({ 
-              responCode: 200,
-              status: 'success',
-              message: 'Berhasil tambah data!'
+            responCode: 200,
+            status: 'success',
+            message: 'Berhasil tambah data!',
+            payloads: results 
         });
-
-      }
-
-    } catch (error: any) {
+      });
+    
+    } catch (error:any) {
       res.status(505).send({ 
-        responCode: 500,
+        responCode: 505,
         status: 'failed',
         message: 'Network Error',
-        payloads: error.message
+        payloads: error.message 
       });
     }
   },
--- dist/controller/controller.js
+++ dist/controller/controller.js
@@ -10,6 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_validator_1 = require("express-validator");
+const connection_1 = require("../connection");
 let models = [];
 const collectData = (id, type, name, details, jumlah) => ({
     id,
@@ -21,22 +22,26 @@ const collectData = (id, type, name, details, jumlah) => ({
 const expenseController = {
     getExpense: (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            if (models.length <= 0) {
-                res.status(200).send({
-                    responCode: 200,
-                    status: 'success',
-                    message: 'Data empty',
-                    payloads: models
-                });
-            }
-            else {
-                res.status(200).send({
-                    responCode: 200,
-                    status: 'success',
-                    message: 'Success get data',
-                    payloads: models
-                });
-            }
+            connection_1.connection.query(`SELECT * FROM tb_expense;`, function (error, results) {
+                if (error)
+                    throw error;
+                if (results.length > 0) {
+                    res.status(200).send({
+                        responCode: 200,
+                        status: 'success',
+                        message: 'Berhasil ambil data!',
+                        payloads: results
+                    });
+                }
+                else {
+                    res.status(404).send({
+                        responCode: 404,
+                        status: 'success',
+                        message: 'Data not found',
+                        payloads: results
+                    });
+                }
+            });
         }
         catch (error) {
             res.status(505).send({
@@ -50,21 +55,40 @@ const expenseController = {
     getExpensebyId: (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         try {
             let ids = req.params.id;
-            let objIndex = models.findIndex((obj => obj.id == `${ids}`));
-            if (objIndex !== -1) {
-                res.status(200).send({
-                    responCode: 200,
-                    status: 'success',
-                    payloads: models[objIndex]
-                });
-            }
-            else {
-                res.status(404).send({
-                    responCode: 404,
-                    status: 'success',
-                    message: 'Data not Found'
-                });
-            }
+            connection_1.connection.query(`
+        SELECT 
+          u.id, 
+          u.name, 
+          u.address , 
+        SUM(CASE WHEN e.type = 'Cash In' THEN e.jumlah ELSE 0 END) AS income,
+        SUM(CASE WHEN e.type = 'Cash Out' THEN e.jumlah ELSE 0 END) AS expense
+        FROM tb_users AS u 
+        JOIN tb_expense AS e 
+        ON e.id_user = u.id
+        WHERE e.id_user = ${ids}
+        GROUP BY u.id`, function (error, results) {
+                if (error)
+                    throw error;
+                if (results.length > 0) {
+                    let parsed = JSON.parse(JSON.stringify(results));
+                    let balance = parsed[0].income - parsed[0].expense;
+                    parsed[0]["balance"] = balance;
+                    res.status(200).send({
+                        responCode: 200,
+                        status: 'success',
+                        message: 'Berhasil ambil data!',
+                        payloads: parsed
+                    });
+                }
+                else {
+                    res.status(404).send({
+                        responCode: 404,
+                        status: 'success',
+                        message: 'Data not found',
+                        payloads: results
+                    });
+                }
+            });
         }
         catch (error) {
             res.status(505).send({
@@ -77,26 +101,22 @@ const expenseController = {
     }),
     addExpense: (req, res) => {
         try {
-            const errors = (0, express_validator_1.validationResult)(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({
-                    success: false,
-                    errors: errors.array(),
-                });
-            }
-            else {
-                let id = "id" + Math.random().toString(16).slice(2);
-                models.push(collectData(id, req.body.type == '1' ? 'Cash In' : 'Cash Out', req.body.name, req.body.details, parseInt(req.body.jumlah)));
+            // console.log(req.body)
+            let id = "id" + Math.random().toString(16).slice(2);
+            connection_1.connection.query(`INSERT INTO tb_expense (id,id_user,type, name, details, jumlah) VALUES(?,?,?,?,?,?);`, [id, req.body.id_user, req.body.type == '1' ? 'Cash In' : 'Cash Out', req.body.name, req.body.details, parseInt(req.body.jumlah)], function (error, results) {
+                if (error)
+                    throw error;
                 res.status(200).send({
                     responCode: 200,
                     status: 'success',
-                    message: 'Berhasil tambah data!'
+                    message: 'Berhasil tambah data!',
+                    payloads: results
                 });
-            }
+            });
         }
         catch (error) {
             res.status(505).send({
-                responCode: 500,
+                responCode: 505,
                 status: 'failed',
                 message: 'Network Error',
                 payloads: error.message
